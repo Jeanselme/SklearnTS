7dd6ab695a571c1da7b1635f08ba5b0cd0ccc694
Vincent <vincent.jeanselme@gmail.com>
2019-02-15 17:57:46 -0500

    Add parallel reader and writer for pandas

---
+++ tests/utils/test_io.py
@@ -0,0 +1,27 @@
+import unittest
+import numpy as np
+import pandas as pd
+from utils.io import *
+
+class TestIo(unittest.TestCase):
+
+    def setUp(self):
+        self.number_points = 10
+        self.number_classes = 3
+        self.dim = 10
+        self.data = {str(j): pd.DataFrame(np.random.rand(np.random.randint(10, 100), self.dim)) for j in range(self.number_points)}
+        self.path = "tests/fake_data/"
+
+    def test_writeParallel(self):
+        writeParallel(self.data, self.path, 3)
+
+    def test_readParallel(self):
+        writeParallel(self.data, self.path, 3)
+        data = readParallel(self.path, 3)
+        self.assertEqual(len(data), len(self.data))
+        for d in data:
+            self.assertEqual(len(data[d]), len(self.data[d]))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ utils/io.py
@@ -0,0 +1,34 @@
+import os
+import pandas as pd
+from joblib import Parallel, delayed
+
+def readParallel(directory, processor, files = None, **args):
+    """
+        Reads files located in the given directory in parallel
+        
+        Arguments:
+            directory {str} -- Path where to read the data
+            processor {int} -- Number of processor to use (see joblib Parallel for more info)
+            files {List of str} -- Names of the files to read (default {None} - Open all files in directory)
+            **args -- Args to give to pandas.read_csv
+
+        Returns:
+            Dict: Keys are file names, Values are pandas objects
+    """
+    if files is None:
+        files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f)) and '.csv' in f]
+    res = Parallel(n_jobs = processor)(delayed(pd.read_csv)(os.path.join(directory, f), **args) for f in files)
+    return {f[:f.index('.csv')]: res[i] for i, f in enumerate(files)}
+
+def writeParallel(timeseries, directory, processor, **args):
+    """
+        Saves the different time series in the given directory
+        
+        Arguments:
+            timeseries {Dict of pandas dataframe} -- Data to save (keys will be the name of the file to save)
+            directory {str} -- Path where to save the data
+            processor {int} -- Number of processor to use (see joblib Parallel for more info)
+    """
+    def write(df, path, **args):
+        df.to_csv(path, **args)
+    Parallel(n_jobs = processor)(delayed(write)(timeseries[ts], os.path.join(directory, ts if '.csv' in ts else ts + '.csv'), **args) for ts in timeseries)
