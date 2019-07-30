6ba80dfadda4aa824b5c2fe27f8e1592098bf56d
Vincent <vincent.jeanselme@gmail.com>
2019-07-30 13:39:21 -0400

    Correction AUC computation

--- metrics/roc.py
+++ metrics/roc.py
@@ -74,7 +74,7 @@ def rocPlot(predictions, truth, classes = None, label = "Model", newFigure = Non
     print(str_print + " +/- {:.2f}".format(np.interp(0.01, newx, wilson)))
     upper = np.minimum(y + wilson, 1)
     lower = np.maximum(y - wilson, 0)
-    plRoc = plt.plot(newx, y, label=label + " ({:.2f} +/- {:.2f})".format(auc(newx, y), (auc(newx, upper) - auc(newx, lower))/2.), ls = '--' if "train" in label.lower() else '-')
+    plRoc = plt.plot(newx, y, label=label + " ({:.2f} +/- {:.2f})".format(aucCompute(predictions, truth, classes), (auc(newx, upper) - auc(newx, lower))/2.), ls = '--' if "train" in label.lower() else '-')
     plt.fill_between(newx, lower, upper, color=plRoc[0].get_color(), alpha=.2)
 
 def computeEvolutionRoc(temporalListLabels, predictions, classes = None, percentage = 0.001):
